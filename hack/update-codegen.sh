e723055f1001084ce53085e4fc77c452e2eb40d9
root <root@localhost.localdomain>
2024-11-25 17:57:27 +0800

    update

--- hack/update-codegen.sh
+++ hack/update-codegen.sh
@@ -23,7 +23,7 @@ CODEGEN_PKG=${CODEGEN_PKG:-$(cd "${SCRIPT_ROOT}"; ls -d -1 ./vendor/k8s.io/code-
 
 source "${CODEGEN_PKG}/kube_codegen.sh"
 
-THIS_PKG="k8s.io/sample-controller"
+THIS_PKG="crds"
 
 kube::codegen::gen_helpers \
     --boilerplate "${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
