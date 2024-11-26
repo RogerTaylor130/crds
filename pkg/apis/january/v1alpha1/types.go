ce4b1e9f9bf9d543a29f12347683c704d9a8ebf1
roger <roger.fan@scaleflux.com>
2024-11-26 09:47:44 +0800

    add replicas in crd; update types

--- pkg/apis/january/v1alpha1/types.go
+++ pkg/apis/january/v1alpha1/types.go
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package twoPodsCrd
+package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -23,32 +23,32 @@ import (
 // +genclient
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// Foo is a specification for a Foo resource
-type Foo struct {
+// January is a specification for a January resource
+type January struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec   FooSpec   `json:"spec"`
-	Status FooStatus `json:"status"`
+	Spec JanuarySpec `json:"spec"`
+	//Status JanuaryStatus `json:"status"`
 }
 
 // FooSpec is the spec for a Foo resource
-type FooSpec struct {
+type JanuarySpec struct {
 	DeploymentName string `json:"deploymentName"`
 	Replicas       *int32 `json:"replicas"`
 }
 
-// FooStatus is the status for a Foo resource
-type FooStatus struct {
-	AvailableReplicas int32 `json:"availableReplicas"`
-}
+//// FooStatus is the status for a Foo resource
+//type FooStatus struct {
+//	AvailableReplicas int32 `json:"availableReplicas"`
+//}
 
 // +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
 
-// FooList is a list of Foo resources
-type FooList struct {
+// JanuaryList is a list of Foo resources
+type JanuaryList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata"`
 
-	Items []Foo `json:"items"`
+	Items []January `json:"items"`
 }
