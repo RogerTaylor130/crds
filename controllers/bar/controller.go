6d1972da274166082279169da2237520f147f82e
roger <roger.fan@scaleflux.com>
2024-11-26 17:24:51 +0800

    mv current logic to controller

--- controllers/bar/controller.go
+++ controllers/bar/controller.go
@@ -3,10 +3,13 @@ package bar
 import (
 	"context"
 	clientset "crds/pkg/generated/clientset/versioned"
+	informers "crds/pkg/generated/informers/externalversions"
 	"golang.org/x/time/rate"
+	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"log"
 	"time"
 )
 
@@ -17,6 +20,7 @@ type BarController struct {
 }
 
 func NewBarController(
+	ctx context.Context,
 	kubeInterface kubernetes.Interface,
 	BarInterface clientset.Interface) *BarController {
 
@@ -31,10 +35,52 @@ func NewBarController(
 		workqueue:     workqueue.NewTypedRateLimitingQueue(ratelimiter),
 	}
 
+	officalFactory := kubeinformers.NewSharedInformerFactory(kubeInterface, time.Second*20)
+	barFactory := informers.NewSharedInformerFactory(BarInterface, 0)
+
+	barsInformer := barFactory.Roger().V1alpha1().Bars()
+	deploymentInformer := officalFactory.Apps().V1().Deployments()
+	podInformer := officalFactory.Core().V1().Pods()
+
+	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			//log.Println("Add pod")
+		},
+	})
+
+	barsInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			log.Println("Add mycrds:", obj)
+		},
+	})
+
+	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			//log.Println("Add deployment:")
+		},
+	})
+
+	log.Println("Starting Informers")
+	officalFactory.Start(ctx.Done())
+	barFactory.Start(ctx.Done())
+
+	log.Println("Waiting for pod&deployment cache sync")
+	if ok := cache.WaitForCacheSync(ctx.Done(), podInformer.Informer().HasSynced, deploymentInformer.Informer().HasSynced); !ok {
+		log.Fatal("Failed to wait for pod or deploymentcache")
+	}
+	log.Println("Pod & deployment cache synced")
+
+	log.Println("Waiting for example cache sync")
+	if ok := cache.WaitForCacheSync(ctx.Done(), barsInformer.Informer().HasSynced); !ok {
+		log.Fatal("Failed to sync")
+	}
+	log.Println("Cache synced")
+
 	return controller
 
 }
 
 func (c BarController) Run(ctx context.Context) {
 
+	<-ctx.Done()
 }
--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package main
 
 import (
-	informers "crds/pkg/generated/informers/externalversions"
+	"crds/controllers/bar"
 	clientTools "crds/tools/client"
-	kubeinformers "k8s.io/client-go/informers"
-	"k8s.io/client-go/tools/cache"
-	"log"
-	"time"
 )
 
 func main() {
@@ -17,47 +13,8 @@ func main() {
 
 	barClient := clientTools.GetExampleClientSet()
 
-	officalFactory := kubeinformers.NewSharedInformerFactory(officalClient, time.Second*20)
-	barFactory := informers.NewSharedInformerFactory(barClient, 0)
+	controller := bar.NewBarController(ctx, officalClient, barClient)
 
-	barsInformer := barFactory.Roger().V1alpha1().Bars()
-	deploymentInformer := officalFactory.Apps().V1().Deployments()
-	podInformer := officalFactory.Core().V1().Pods()
-
-	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			log.Println("Add pod")
-		},
-	})
-
-	barsInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			log.Println("Add mycrds:", obj)
-		},
-	})
-
-	deploymentInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			log.Println("Add deployment:", obj)
-		},
-	})
-
-	log.Println("Starting Informers")
-	officalFactory.Start(ctx.Done())
-	barFactory.Start(ctx.Done())
-
-	log.Println("Waiting for pod&deployment cache sync")
-	if ok := cache.WaitForCacheSync(ctx.Done(), podInformer.Informer().HasSynced, deploymentInformer.Informer().HasSynced); !ok {
-		log.Fatal("Failed to wait for pod or deploymentcache")
-	}
-	log.Println("Pod & deployment cache synced")
-
-	log.Println("Waiting for example cache sync")
-	if ok := cache.WaitForCacheSync(ctx.Done(), barsInformer.Informer().HasSynced); !ok {
-		log.Fatal("Failed to sync")
-	}
-	log.Println("Cache synced")
-
-	<-time.After(time.Minute * 5)
+	controller.Run(ctx)
 
 }
