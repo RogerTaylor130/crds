d4a457d00ba11d3178da23e5cdaa2659a7fbe181
roger <roger.fan@scaleflux.com>
2024-11-26 18:12:51 +0800

    save

--- controllers/bar/controller.go
+++ controllers/bar/controller.go
@@ -4,25 +4,36 @@ import (
 	"context"
 	clientset "crds/pkg/generated/clientset/versioned"
 	informers "crds/pkg/generated/informers/externalversions"
+	"crds/pkg/generated/informers/externalversions/mycrds/v1alpha1"
 	"golang.org/x/time/rate"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
 	kubeinformers "k8s.io/client-go/informers"
+	v1 "k8s.io/client-go/informers/apps/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
 	"log"
 	"time"
 )
 
 type BarController struct {
-	kubeInterface kubernetes.Interface
-	BarInterface  clientset.Interface
-	workqueue     workqueue.TypedRateLimitingInterface[cache.ObjectName]
+	kubeInterface      kubernetes.Interface
+	BarInterface       clientset.Interface
+	barInformer        v1alpha1.BarInformer
+	deploymentInformer v1.DeploymentInformer
+	workqueue          workqueue.TypedRateLimitingInterface[cache.ObjectName]
 }
 
 func NewBarController(
 	ctx context.Context,
 	kubeInterface kubernetes.Interface,
-	BarInterface clientset.Interface) *BarController {
+	barInterface clientset.Interface,
+	barInformer v1alpha1.BarInformer,
+	deploymentInformer v1.DeploymentInformer) *BarController {
+
+	logger := klog.FromContext(ctx)
 
 	ratelimiter := workqueue.NewTypedMaxOfRateLimiter(
 		workqueue.NewTypedItemExponentialFailureRateLimiter[cache.ObjectName](5*time.Millisecond, 1000*time.Second),
@@ -31,26 +42,32 @@ func NewBarController(
 
 	controller := &BarController{
 		kubeInterface: kubeInterface,
-		BarInterface:  BarInterface,
+		BarInterface:  barInterface,
 		workqueue:     workqueue.NewTypedRateLimitingQueue(ratelimiter),
 	}
 
 	officalFactory := kubeinformers.NewSharedInformerFactory(kubeInterface, time.Second*20)
-	barFactory := informers.NewSharedInformerFactory(BarInterface, 0)
+	barFactory := informers.NewSharedInformerFactory(barInterface, 0)
 
-	barsInformer := barFactory.Roger().V1alpha1().Bars()
-	deploymentInformer := officalFactory.Apps().V1().Deployments()
-	podInformer := officalFactory.Core().V1().Pods()
+	//barsInformer := barFactory.Roger().V1alpha1().Bars()
 
-	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+	//podInformer := officalFactory.Core().V1().Pods()
+	//
+	//podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+	//	AddFunc: func(obj interface{}) {
+	//		//log.Println("Add pod")
+	//	},
+	//})
+
+	barInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			//log.Println("Add pod")
+			controller.enqueue(obj)
 		},
-	})
-
-	barsInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: func(obj interface{}) {
-			log.Println("Add mycrds:", obj)
+		UpdateFunc: func(old, new interface{}) {
+			controller.enqueue(new)
+		},
+		DeleteFunc: func(obj interface{}) {
+			logger.Info("Bar Crd deleted: ", "Object:", obj)
 		},
 	})
 
@@ -64,23 +81,44 @@ func NewBarController(
 	officalFactory.Start(ctx.Done())
 	barFactory.Start(ctx.Done())
 
+	return controller
+
+}
+
+func (c BarController) Run(ctx context.Context, worker int) {
+
 	log.Println("Waiting for pod&deployment cache sync")
-	if ok := cache.WaitForCacheSync(ctx.Done(), podInformer.Informer().HasSynced, deploymentInformer.Informer().HasSynced); !ok {
+	if ok := cache.WaitForCacheSync(ctx.Done(), c.deploymentInformer.Informer().HasSynced); !ok {
 		log.Fatal("Failed to wait for pod or deploymentcache")
 	}
 	log.Println("Pod & deployment cache synced")
 
 	log.Println("Waiting for example cache sync")
-	if ok := cache.WaitForCacheSync(ctx.Done(), barsInformer.Informer().HasSynced); !ok {
+	if ok := cache.WaitForCacheSync(ctx.Done(), c.barInformer.Informer().HasSynced); !ok {
 		log.Fatal("Failed to sync")
 	}
 	log.Println("Cache synced")
 
-	return controller
-
+	for i := 0; i < worker; i++ {
+		go wait.UntilWithContext(ctx, c.runWorker, time.Second)
+	}
 }
 
-func (c BarController) Run(ctx context.Context) {
+func (c BarController) runWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
 
-	<-ctx.Done()
+func (c BarController) processNextWorkItem(ctx context.Context) bool {
+	logger := klog.FromContext(ctx)
+
+	return true
+}
+
+func (c BarController) enqueue(obj interface{}) {
+	if objRef, err := cache.ObjectToName(obj); err != nil {
+		utilruntime.HandleError(err)
+	} else {
+		c.workqueue.Add(objRef)
+	}
 }
