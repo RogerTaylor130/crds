e7e0e0f90067b0c1ab2c7bda434fe144e5540bc0
LongKang Fan <fanlongkang@qq.com>
2024-12-06 17:29:51 +0800

    save

--- pkg/apis/webapp/v1alpha1/register.go
+++ pkg/apis/webapp/v1alpha1/register.go
@@ -25,28 +25,37 @@ import (
 // SchemeGroupVersion is group version used to register these objects
 var SchemeGroupVersion = schema.GroupVersion{Group: "roger.alpha.example.com", Version: "v1alpha1"}
 
-// Kind takes an unqualified kind and returns back a Group qualified GroupKind
-func Kind(kind string) schema.GroupKind {
-	return SchemeGroupVersion.WithKind(kind).GroupKind()
+var (
+	SchemeBuilder      runtime.SchemeBuilder
+	localSchemeBuilder = &SchemeBuilder
+	AddToScheme        = localSchemeBuilder.AddToScheme
+)
+
+func init() {
+	// We only register manually written functions here. The registration of the
+	// generated functions takes place in the generated files. The separation
+	// makes the code compile even when the generated files are missing.
+	localSchemeBuilder.Register(addKnownTypes)
 }
+
+//// Kind takes an unqualified kind and returns back a Group qualified GroupKind
+//func Kind(kind string) schema.GroupKind {
+//	return SchemeGroupVersion.WithKind(kind).GroupKind()
+//}
 
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
-var (
-	// SchemeBuilder initializes a scheme builder
-	SchemeBuilder = runtime.NewSchemeBuilder(addKnownTypes)
-	// AddToScheme is a global function that registers this API group & version to a scheme
-	AddToScheme = SchemeBuilder.AddToScheme
-)
-
 // Adds the list of known types to Scheme.
 func addKnownTypes(scheme *runtime.Scheme) error {
 	scheme.AddKnownTypes(SchemeGroupVersion,
-		&Bar{},
-		&BarList{},
+		&Webapp{},
+		&WebappList{},
+	)
+	scheme.AddKnownTypes(SchemeGroupVersion,
+		&metav1.Status{},
 	)
 	metav1.AddToGroupVersion(scheme, SchemeGroupVersion)
 	return nil
